25d0ae2a8ad90ba47b6c1c4cc959a02e8218f110
ErikPelli <erikpellizzon1@gmail.com>
2022-12-06 18:15:49 +0100

    Initialize Spring gradle project

---
+++ src/main/java/me/erikpelli/bigmetric/BigMetricApplication.java
@@ -0,0 +1,13 @@
+package me.erikpelli.bigmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigMetricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigMetricApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/erikpelli/bigmetric/BigMetricApplicationTests.java
@@ -0,0 +1,13 @@
+package me.erikpelli.bigmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMetricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
