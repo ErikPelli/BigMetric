0c7e0110a42a6a2d6749ad6308370d2d69e0d6bd
ErikPelli <erikpellizzon1@gmail.com>
2022-12-15 19:13:13 +0100

    KafkaSender class to send the temperature
    Use @EmbeddedKafka in the test

---
+++ src/main/java/me/erikpelli/bigmetric/thermometer/sender/KafkaSender.java
@@ -0,0 +1,37 @@
+package me.erikpelli.bigmetric.thermometer.sender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+class KafkaSender {
+    private final KafkaTemplate<String, KafkaData> kafkaTemplate;
+    private final String temperatureTopic;
+
+    @Autowired
+    KafkaSender(KafkaTemplate<String, KafkaData> kafkaTemplate, @Value("${kafka.topicName}") String temperatureTopic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.temperatureTopic = temperatureTopic;
+    }
+
+    /**
+     * Send a generic message using kafka.
+     *
+     * @param topicName kafka topic to use
+     * @param data      data to send
+     */
+    void sendMessage(String topicName, KafkaData data) {
+        kafkaTemplate.send(topicName, data);
+    }
+
+    /**
+     * Send temperature data using the preconfigured Kafka topic.
+     *
+     * @param data temperature data to send
+     */
+    void sendTemperature(KafkaData data) {
+        sendMessage(this.temperatureTopic, data);
+    }
+}
---
+++ src/test/java/me/erikpelli/bigmetric/thermometer/sender/KafkaSenderTest.java
@@ -0,0 +1,28 @@
+package me.erikpelli.bigmetric.thermometer.sender;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+class KafkaSenderTest {
+
+    @Autowired
+    private KafkaSender sender;
+
+    @Test
+    @Timeout(value = 10)
+    void sendTemperature() {
+        var data = new KafkaData(1, 1234, 50.0, "1234");
+        assertDoesNotThrow(() -> {
+            sender.sendTemperature(data);
+        });
+    }
+}
